f2f7de9f573c8080b7c7cfa48affdc1972fa9bc0
Martin <martin.mejlaender.larsen@gmail.com>
2026-07-11 08:52:44 +0200

    oppdater kort

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /* GENERERT av publiser.py — ikke rediger for hånd, endre sky/config.json */
 window.KONFIG = {
   kortUrl: "https://martinmejlaenderlarsen.github.io/rex-kort/kort.json",
-  svarUrl: ""
+  svarUrl: "https://script.google.com/macros/s/AKfycbyqIvoscl7hnoUUyfGlcuiSaatkMK8QsOQ6peF8TOTaHDXsMmx_2SLUOUbw2esoMcri/exec"
 };
